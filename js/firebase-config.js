444b0d31117242056464026676199f2167d52868
kj-1100 <kaykyjcm@gmail.com>
2025-04-23 17:48:48 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,3 @@
-// Configuração do Firebase (substitua com seus dados)
-const firebaseConfig = {
-    apiKey: "AIzaSyDb0eijnKInu9YfvHgBaTt9ap1otLL_v1k",
-    authDomain: "trabalhofinal-43049.firebaseapp.com",
-    projectId: "trabalhofinal-43049",
-    storageBucket: "trabalhofinal-43049.firebasestorage.app",
-    messagingSenderId: "97029804425",
-    appId: "1:97029804425:web:03ef0226c7925887975158",
-    measurementId: "G-QKDBP5JJW8"
-};
-
 // Inicializa o Firebase
 const app = firebase.initializeApp(window._FIREBASE_CONFIG);
 const auth = firebase.auth(app);
